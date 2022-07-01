9bad846185a9a14ba9cb2c25d5742c12e28c73ad
Spencer Brian <brianbaiden@gmail.com>
2022-07-01 19:10:29 +0000

    Inclusion of different game levels

--- Guess_the_number/app.py
+++ Guess_the_number/app.py
@@ -1,24 +1,43 @@
 from random import randint
 
-print("Welcome to RECNEPS MINI GUESS GAME")
+def welcome(): 
+    print("Welcome to RECNEPS MINI GUESS GAME\n")
+    print("Easy - 1")
+    print("Intermediate - 2")
+    print("Hard - 3")
+
+    difficulty = int(input("Please enter a difficulty from 1 to 3: \n"))
+    if difficulty == 1:
+        easy()
+    elif difficulty == 2:
+        Intermediate()
+    elif difficulty == 3:
+        Hard()
+    else:
+        print("Wrong input")
+        welcome()
+
+
 
 hint_higher = "The number is higher than this"
 hint_lower = "The number is lower than this"
 final_hint_higher = "The number is higher than this"
 final_hint_lower =  "The number is lower than this"
-game_number = randint(0,10)
+easy_game_number = randint(0,10)
+intermediate_game_number = randint(0,100)
+hard_game_number = randint(0,1000)
 winner = "You won"
 loser = "Sorry, Try again"
 
-def game():
+def easy():
     attempt = 3
     while attempt > 0:
         try:
-            guess = int(input('Enter a number: \n'))
-            if guess == game_number:
+            guess = int(input('Enter a number from 0 to 10: \n'))
+            if guess == easy_game_number:
                 print(winner)
                 break
-            elif guess > game_number:
+            elif guess > easy_game_number:
                 if attempt == 2:
                     print(f"Last try and {final_hint_lower}")
                     attempt -= 1
@@ -26,7 +45,7 @@ def game():
                     print(hint_lower)
                     attempt -= 1
                     continue
-            elif guess < game_number:
+            elif guess < easy_game_number:
                 if attempt == 2:
                     print(f"Last try and {final_hint_higher}")
                     attempt -= 1
@@ -37,14 +56,79 @@ def game():
         except ValueError:
             print("Sorry, this game accepts integers only")
     
-    if guess == game_number:
+    if guess == easy_game_number:
         print("Congratulations you beat the game")
     else:
         print("Sorry, you've run out of guesses")
-    print(f"Also the right answer was {game_number}")
+        print(f"Also the right answer was {easy_game_number}")
+
+def Intermediate():
+    attempt = 5
+    while attempt > 0:
+        try:
+            guess = int(input('Enter a number from 0 to 100: \n'))
+            if guess == intermediate_game_number:
+                print(winner)
+                break
+            elif guess > intermediate_game_number:
+                if attempt == 2:
+                    print(f"Last try and {final_hint_lower}")
+                    attempt -= 1
+                else:
+                    print(hint_lower)
+                    attempt -= 1
+                    continue
+            elif guess < intermediate_game_number:
+                if attempt == 2:
+                    print(f"Last try and {final_hint_higher}")
+                    attempt -= 1
+                else:
+                    print(hint_higher)
+                    attempt -= 1
+                    continue
+        except ValueError:
+            print("Sorry, this game accepts integers only")
+    if guess == intermediate_game_number:
+        print("Congratulations you beat the game")
+    else:
+        print("Sorry, you've run out of guesses")
+        print(f"Also the right answer was {intermediate_game_number}")
+
+def Hard():
+    attempt = 10
+    while attempt > 0:
+        try:
+            guess = int(input('Enter a number from 0 to 1000: \n'))
+            if guess == hard_game_number:
+                print(winner)
+                break
+            elif guess > hard_game_number:
+                if attempt == 2:
+                    print(f"Last try and {final_hint_lower}")
+                    attempt -= 1
+                else:
+                    print(hint_lower)
+                    attempt -= 1
+                    continue
+            elif guess < hard_game_number:
+                if attempt == 2:
+                    print(f"Last try and {final_hint_higher}")
+                    attempt -= 1
+                else:
+                    print(hint_higher)
+                    attempt -= 1
+                    continue
+        except ValueError:
+            print("Sorry, this game accepts integers only")
+    
+    if guess == hard_game_number:
+        print("Congratulations you beat the game")
+    else:
+        print("Sorry, you've run out of guesses")
+        print(f"Also the right answer was {hard_game_number}")
 
 def main():
-    game()
+    welcome()
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -3,4 +3,12 @@ This is a repository for all projects I have worked on to improve my python skil
 Below is a list of all the projects in this repo.
 1. Guess_the_number.
     # Instructions
-    This is one of the simple python projects yet an exciting one. You can even call it a mini-game. This project is particularly useful for beginners. Make a program in which the computer randomly chooses a number between 1 to 10, 1 to 100, or any range. Then give users a hint to guess the number. Every time the user guesses wrong, he gets another clue, and his score gets reduced. The clue can be multiples, divisible, greater or smaller, or a combination of all.You will also need functions to compare the inputted number with the guessed number, to compute the difference between the two, and to check whether an actual number was inputted or not in this python project. The main aim of this coding project idea from the Python projects list is to introduce beginners to coding basics.
+    This is a simple guessing game where there is a random number generated from a range depending on the game difficulty(easy, intermediate or hard). If the input value for the game level or guess is not accounted for, an error message shows up. For every level a specific number of attempts are allowed that you can keep guessing else it's game over. All three levels have been divided into three different functions with separate while loops so I could easily debug every level. The game also includes hints for when the number guessed is higher or lower than the random number that has been generated for the game level.
+    {
+        if guess == easy_game_number:
+            print("Congratulations you beat the game")
+        else:
+            print("Sorry, you've run out of guesses")
+            print(f"Also the right answer was {easy_game_number}")
+    }
+    Depending on wether the user gets the guess right or runs out of attempts, the game comes to an end and the right answer is displayed.
