06015d12f237b2cb3f95e55a6d256633b5a3ed6e
Spencer Brian <brianbaiden@gmail.com>
2022-07-04 22:44:25 +0000

    Completion of hangman game(Addition of images) and update README

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,8 +1,9 @@
 import random
+from images import hangman_images
 from words import words
 
-yes = ["Yes","yes", "Y", "y"]
-no = ["No", "no", "N", "n"]
+yes = ["Yes","yes", "Y", "y", "YES", "YeS", "yEs", "YEs", "yeS", "yES"]
+no = ["No", "no", "N", "n", "nO", "NO"]
 def welcome():
     print("Welcome to the Hangman Game")
     play_option = input("To play, please enter yes or enter no to quit.\n")
@@ -24,43 +25,43 @@ def hangman():
         final_random_word.append(index_letter[1])
     # print(final_random_word)
     print(word_to_guess)
-    attempts = 6
+    attempts = 7
+    mistakes = 0
     while attempts > 0:
-        gues = input("Please enter a letter.\n")
-        guess = gues.upper()
+        guess = (input("Please enter a letter.\n")).upper()
         if guess.isalpha and len(guess) == 1:
-                if guess in final_random_word:
-                    print(f"Correct. {guess} is in this word")
-                    if guess in guessed_word:
-                        print("You have already guessed this.")
-                    else:
-                        guessed_word.append(guess)
-                        list_word_to_guess = list(word_to_guess)
-                        print(list_word_to_guess)
-                        # If the letter in {random_word} == guess, append the letter in indices. This does it at the same place
-                        indices = [i for i, letter in enumerate(random_word)
-                        if letter == guess]
-                        for index in indices:
-                            list_word_to_guess[index] = guess
+            if guess in final_random_word:
+                print(f"Correct. {guess} is in this word")
+                if guess in guessed_word:
+                    print("You have already guessed this.")
+                else:
+                    guessed_word.append(guess)
+                    list_word_to_guess = list(word_to_guess)
+                    # If the letter in {random_word} == guess, append the letter in indices. This does it at the same place
+                    indices = [i for i, letter in enumerate(random_word)
+                    if letter == guess]
+                    for index in indices:
+                        list_word_to_guess[index] = guess
                         word_to_guess = "".join(list_word_to_guess)
                         print(word_to_guess)
-                    attempts -= 1
-                    if "_" not in word_to_guess:
-                        print(f"Congratulations! You won with {attempts} left")
-                        play_again()
-                    continue
-                else:
-                    print("Bad try! Try again.")
-                    attempts -= 1
-                    continue
+                        attempts -= 1
+                        if "_" not in word_to_guess:
+                            print(f"Congratulations! You won with {attempts} left")
+                            play_again()
+                            continue
+            else:
+                print("Bad try! Try again.")
+                mistakes += 1
+                attempts -= 1
+                hang = hangman_images[(0+mistakes)]
+                print(hang)
+                continue
         else:
             print("Sorry, wrong input.")
             continue
     if attempts < 1 and ("_" in word_to_guess):
         print("You lost!")
         welcome()
-    elif attempts < 1 and (len("_" in word_to_guess) == 0):
-        print("Wow you somehow won. Congratulations!")
 
 def play_again():
     print("Do you want to play again?\n")
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## PYTHON PROJECTS
 This is a repository for all projects I have worked on to improve my python skills as a developer.
 Below is a list of all the projects in this repo.
-1. Guess The Number.
-    ## Instructions
+1. ## Guess The Number.
     This is a simple guessing game where there is a random number generated from a range depending on the game difficulty(easy, intermediate or hard). If the input value for the game level or guess is not accounted for, an error message shows up. For every level a specific number of attempts are allowed that you can keep guessing else it's game over. All three levels have been divided into three different functions with separate while loops so I could easily debug every level. The game also includes hints for when the number guessed is higher or lower than the random number that has been generated for the game level.<br>
     {<br>
         if guess == easy_game_number:<br>
@@ -13,6 +12,8 @@ Below is a list of all the projects in this repo.
     }<br>
     Depending on wether the user gets the guess right or runs out of attempts, the game comes to an end and the right answer is displayed.
 
-2. Rolling Dice Simulator
-    ## Instructions
+2. ## ROLLING DICE SIMULATOR
     Yet another simple game but takes than an hour to complete. In this game a user just enters yes or no to roll the die or quit the game respectively. If the user enters yes, randint is used to print a random integer from 1 to 6. The game has two levels; one for a single die roll and another for a dice roll which returns a sum of the dice.
+
+3. ## HANGMAN
+    After eight rounds of coffee and a lot of copy and pasting and some modification of course. I finished the hangman game. Trust, I thought I would bail on this. Anyway, it's your regular hangman game with some words which are just two in this case apple and orange. The basic functionality of the game being able to detect a letter and pass it as a guess and crosscheck with the random word selected is working fine. Also being able to replace the a letter than still needs guessing is an added functionality.
