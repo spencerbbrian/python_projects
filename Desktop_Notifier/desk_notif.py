9178a3d9e57d714c553e8a2931ae5285a23c5b68
Spencer Brian <brianbaiden@gmail.com>
2022-07-22 21:15:21 +0000

    Basic structure of GUI

--- Desktop_Notifier/desk_notif.py
+++ Desktop_Notifier/desk_notif.py
@@ -1,2 +1,22 @@
 from tkinter import *
 from PIL import ImageTk, Image
+
+root = Tk()
+root.title("DESKTOP NOTIFIER")
+root.iconbitmap(r'C:\Users\brian\Desktop\CCR\FULLSTACK_DEVELOPMENT\BACKEND\PYTHON\Projects\Desktop_Notifier\alert_icon.ico')
+root.geometry('400x400')
+
+#ENTRY BOXES
+title_to_notify = Entry(root,width=35).grid(row=0,column=1)
+message = Entry(root,width=35).grid(row=1, column=1)
+time = Entry(root,width=10).grid(row=2,column=1)
+
+#ENTRY BOXES LABELS
+title_to_notify_label = Label(root,text="title to notify")
+title_to_notify_label.grid(row=0,column=0,pady=10)
+message_label = Label(root,text="message")
+message_label.grid(row=1,column=0)
+time_label = Label(root,text="time")
+time_label.grid(row=2,column=0)
+
+root.mainloop()
