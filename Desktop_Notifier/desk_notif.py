7bf2ed341e3bac75a41e6b1d8dc47beef9db1fb9
Spencer Brian <brianbaiden@gmail.com>
2022-07-25 23:46:06 +0000

    Final GUI created with button commented out.

--- Desktop_Notifier/desk_notif.py
+++ Desktop_Notifier/desk_notif.py
@@ -1,22 +1,44 @@
 from tkinter import *
+from plyer import notification
 from PIL import ImageTk, Image
 
-root = Tk()
-root.title("DESKTOP NOTIFIER")
-root.iconbitmap(r'C:\Users\brian\Desktop\CCR\FULLSTACK_DEVELOPMENT\BACKEND\PYTHON\Projects\Desktop_Notifier\alert_icon.ico')
-root.geometry('400x400')
+t = Tk()
+t.title("DESKTOP NOTIFIER")
+t.iconbitmap(r'C:\Users\brian\Desktop\CCR\FULLSTACK_DEVELOPMENT\BACKEND\PYTHON\Projects\Desktop_Notifier\alert_icon.ico')
+t.geometry('400x400')
 
-#ENTRY BOXES
-title_to_notify = Entry(root,width=35).grid(row=0,column=1)
-message = Entry(root,width=35).grid(row=1, column=1)
-time = Entry(root,width=10).grid(row=2,column=1)
+# Label - Title
+t_label = Label(t, text="Title to Notify",font=("poppins", 10))
+t_label.place(x=12, y=70)
 
-#ENTRY BOXES LABELS
-title_to_notify_label = Label(root,text="title to notify")
-title_to_notify_label.grid(row=0,column=0,pady=10)
-message_label = Label(root,text="message")
-message_label.grid(row=1,column=0)
-time_label = Label(root,text="time")
-time_label.grid(row=2,column=0)
+# ENTRY - Title
+title = Entry(t, width="40",font=("poppins", 13))
+title.place(x=140, y=70)
 
-root.mainloop()
+# Label - Message
+m_label = Label(t, text="Display Message", font=("poppins", 10))
+m_label.place(x=12, y=120)
+
+# ENTRY - Message
+msg = Entry(t, width="40", font=("poppins", 13))
+msg.place(x=140,height=30, y=120)
+
+# Label - Time
+time_label = Label(t, text="Set Time", font=("poppins", 10))
+time_label.place(x=12, y=175)
+
+# ENTRY - Time
+time1 = Entry(t, width="5", font=("poppins", 13))
+time1.place(x=140, y=175)
+
+# Label - min
+time_min_label = Label(t, text="min", font=("poppins", 10))
+time_min_label.place(x=205, y=180)
+
+# Button
+# but = Button(t, text="SET NOTIFICATION", font=("poppins", 10, "bold"), fg="#ffffff", bg="#528DFF", width=20,
+#              relief="raised",
+#              command=get_details)
+# but.place(x=170, y=230)
+
+t.mainloop()
